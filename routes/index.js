5214b6edc4a65bd0617df11163ecb8f4ee4a79bc
Tiffany <tiffany@Tiffanys-Mac.local>
2020-07-24 09:00:12 -0600

    too many files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var json =[{id:1,name:foo,},{id:2, name:foooo}]
+  res.render('index',{title: 'title', json});
 });
 
 module.exports = router;
